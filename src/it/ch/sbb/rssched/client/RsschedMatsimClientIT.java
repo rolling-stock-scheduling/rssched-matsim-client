d0a453d9a8be93736cd425d6dc3295df112d52a0
Merlin Unterfinger <merlin.unterfinger@sbb.ch>
2024-04-29 18:02:32 +0200

    Add variation in depot and maintenance locations

--- src/it/ch/sbb/rssched/client/RsschedMatsimClientIT.java
+++ src/it/ch/sbb/rssched/client/RsschedMatsimClientIT.java
@@ -19,15 +19,18 @@ class RsschedMatsimClientIT {
     public static final LocalDateTime NIGHT_SHIFT_START = LocalDateTime.of(2020, 1, 1, 20, 0, 0);
     public static final LocalDateTime NIGHT_SHIFT_END = LocalDateTime.of(2020, 1, 2, 4, 0, 0);
     public static final int DEPOT_CAPACITY = 10;
-    public static final int TRACK_COUNT = 2;
+    public static final int MAINTENANCE_TRACK_COUNT = 3;
     private final Set<String> STOPS_IN_KEHLHEIM = Set.of("short_10302.5", "regio_275062", "short_10302.6",
             "regio_289114.2", "regio_289114.4", "regio_92976", "short_10302.3", "short_10302.4", "regio_275062.1",
             "short_9488", "short_10302.1", "short_10302.2", "short_9488.1", "short_9488.4", "short_9488.5",
             "short_9488.2", "short_9488.3", "short_9488.6", "short_10302", "short_12323", "regio_289114",
             "regio_289114.1", "regio_289114.3", "regio_185728");
-    private final Set<String> DEPOT_MAINTENANCE_LOCATIONS = Set.of("regio_141134", "regio_151384", "regio_172317",
-            "regio_215502", "short_1119", "short_3740", "short_5972", "short_6900");
-
+    private final Set<String> ON_ROUTE_SHUNTING_LOCATIONS = Set.of("regio_141134", "regio_166759", "regio_314854",
+            "regio_215502", "short_1119", "short_6474", "short_5972", "short_9804");
+    private final Set<String> DEPOT_LOCATIONS = Set.of("regio_141134", "regio_151384", "regio_172317", "regio_215502",
+            "regio_293170", "short_1119", "short_3740", "short_5972", "short_6900");
+    private final Set<String> MAINTENANCE_LOCATIONS = Set.of("regio_67809", "regio_81147", "regio_293170", "short_9666",
+            "short_9804", "short_1119");
 
     @BeforeEach
     void setUp() throws IOException {
@@ -40,13 +43,12 @@ class RsschedMatsimClientIT {
         FilterStrategy filterStrategy = scenario -> {
             TransitLineSelection selection = new TransitLineSelection();
             scenario.getTransitSchedule().getTransitLines().forEach(
-                    (transitLineId, transitLine) -> transitLine.getRoutes()
-                            .forEach((transitRouteId, transitRoute) -> {
-                                if (transitRoute.getStops().stream().anyMatch(stop -> STOPS_IN_KEHLHEIM.contains(
-                                        stop.getStopFacility().getId().toString()))) {
-                                    selection.add(TransitLineSelection.NO_GROUP, transitLineId, transitRouteId);
-                                }
-                            }));
+                    (transitLineId, transitLine) -> transitLine.getRoutes().forEach((transitRouteId, transitRoute) -> {
+                        if (transitRoute.getStops().stream().anyMatch(
+                                stop -> STOPS_IN_KEHLHEIM.contains(stop.getStopFacility().getId().toString()))) {
+                            selection.add(TransitLineSelection.NO_GROUP, transitLineId, transitRouteId);
+                        }
+                    }));
             return selection;
         };
 
@@ -55,25 +57,31 @@ class RsschedMatsimClientIT {
                 .setOutputDirectory(IT_OUTPUT_DIRECTORY).setRunId(RUN_ID).setFilterStrategy(filterStrategy);
 
         // add depots
-        DEPOT_MAINTENANCE_LOCATIONS.forEach(location -> {
+        DEPOT_LOCATIONS.forEach(location -> {
             String depotId = String.format("dpt_%s", location);
             builder.addDepot(depotId, location, DEPOT_CAPACITY);
-            if (location.startsWith("regio")) {
+            if (location.startsWith("regio_2")) {
+                builder.addAllowedTypeToDepot(depotId, "RE_RB_veh_type", DEPOT_CAPACITY / 2);
+                builder.addAllowedTypeToDepot(depotId, "Bus_veh_type", DEPOT_CAPACITY / 2);
+            } else if (location.startsWith("regio")) {
                 builder.addAllowedTypeToDepot(depotId, "RE_RB_veh_type", DEPOT_CAPACITY);
             } else {
                 builder.addAllowedTypeToDepot(depotId, "Bus_veh_type", DEPOT_CAPACITY);
             }
         });
 
+        // add on route shunting locations
+        ON_ROUTE_SHUNTING_LOCATIONS.forEach(builder::addShuntingLocation);
+
         // add maintenance day shifts
-        DEPOT_MAINTENANCE_LOCATIONS.forEach(
+        MAINTENANCE_LOCATIONS.forEach(
                 location -> builder.addMaintenanceSlot(String.format("mnt_%s_day", location), location, DAY_SHIFT_START,
-                        DAY_SHIFT_END, TRACK_COUNT));
+                        DAY_SHIFT_END, MAINTENANCE_TRACK_COUNT));
 
         // add maintenance night shifts
-        DEPOT_MAINTENANCE_LOCATIONS.forEach(
+        MAINTENANCE_LOCATIONS.forEach(
                 location -> builder.addMaintenanceSlot(String.format("mnt_%s_night", location), location,
-                        NIGHT_SHIFT_START, NIGHT_SHIFT_END, TRACK_COUNT));
+                        NIGHT_SHIFT_START, NIGHT_SHIFT_END, MAINTENANCE_TRACK_COUNT));
 
         // set sample size
         RsschedRequestConfig config = builder.buildWithDefaults();
